171a52558f64027bc8e8550a30b20bd2db34d9f9
vorw <sultanalsanie23@gmail.com>
2025-07-28 16:23:32 +0000

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines Square
+"""
+
+
 class Square:
+    """ empty class for Square"""
+    
     def __init__(Square):
+        """ empty object, returns None"""
         return None
