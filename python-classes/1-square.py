4905d3adb908abcae80e78f0492d35a66a8300f5
vorw <sultanalsanie23@gmail.com>
2025-07-28 16:33:52 +0000

    Task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This module is redfining square..
+"""
+
+
 class Square:
+    """ class of square"""
+
     def __init__(self, size):
+        """
+        object for size
+        """
         self.__size
