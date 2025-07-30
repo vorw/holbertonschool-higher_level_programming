9021cd39a248519e7a233a9e40fa327efc9613c0
vorw <sultanalsanie23@gmail.com>
2025-07-30 16:56:20 +0000

    Task4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,12 +11,13 @@ class Square:
         """
         object for size
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        def size(self, value):
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
 
     def area(self):
         """
