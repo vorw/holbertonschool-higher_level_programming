dfbcd946d61043bc148a37962dc3ee1cbda95d5a
vorw <sultanalsanie23@gmail.com>
2025-08-07 13:59:15 +0300

    Task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module to check instance"""
+
+
+def inherits_from(obj, a_class):
+    """checker"""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return False
+    else:
+        return True
