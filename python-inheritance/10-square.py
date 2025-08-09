9fb83b464ca20d31c6dba4b02cffe441f40379ba
vorw <sultanalsanie23@gmail.com>
2025-08-09 10:04:21 +0000

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module for square
+"""
+
+
+Rectangle = __import__('8-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class of square"""
+
+    def __init__(self, size):
+        """size of square"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area calc"""
+        return self.__size ** 2
