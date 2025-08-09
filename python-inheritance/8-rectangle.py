1f1dd1d15c9cbbf20fd580aa7b42f5d4a8f4b70e
vorw <sultanalsanie23@gmail.com>
2025-08-09 09:49:10 +0000

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module for rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """class of rect"""
+
+    def __init__(self, width, height):
+        """width and height"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
