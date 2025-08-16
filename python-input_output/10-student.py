dd55739627f5ea5a0388a78d59958dee7a717fe3
vorw <sultanalsanie23@gmail.com>
2025-08-16 19:42:48 +0000

    Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+student to json with filter
+"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
