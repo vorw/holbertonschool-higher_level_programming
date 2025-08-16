cc54ae5cbe7aca2d3b2daee265f43b22fe36ec51
vorw <sultanalsanie23@gmail.com>
2025-08-16 19:30:48 +0000

    Task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+student to json
+"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """dict"""
+        self.__dict__
