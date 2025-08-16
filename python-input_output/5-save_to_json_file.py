be79cb2c30b7fd5c9040fe1c64b96f5bc302fdec
vorw <sultanalsanie23@gmail.com>
2025-08-16 19:05:00 +0000

    Task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,6 +8,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-   """main func"""
-   with open(filename, "w", encoding="utf-8") as f:
-      json.dump(my_obj, f)
+    """main func"""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
