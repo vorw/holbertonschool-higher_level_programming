9288e304f0e90680f9b59654b0534f6f4e32341c
vorw <sultanalsanie23@gmail.com>
2025-08-16 11:42:36 +0000

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ reading file
 
 def read_file(filename=""):
     """accessing the file"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         test = f.read()
         print(test)
