910d6991d511d75d2de34ec39fbd1fd397d76b6b
vorw <sultanalsanie23@gmail.com>
2025-08-16 19:44:29 +0000

    Task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -8,6 +8,7 @@ class Student:
     """student class"""
 
     def __init__(self, first_name, last_name, age):
+        """main func"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -16,5 +17,4 @@ class Student:
         """attrs checker"""
         if attrs is None:
             return self.__dict__
-        else:
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
