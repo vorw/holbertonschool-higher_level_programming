bdfcf2f83232044df878ff30239d53bc15bf1620
vorw <sultanalsanie23@gmail.com>
2025-08-23 20:30:53 +0000

    Task 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+class Handler(BaseHTTPRequestHandler):
+    """handler"""
+
+    def do_GET(self):
+        """get"""
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+        
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        
+        elif self.path == "/info":
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(info).encode())
+        
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+server = HTTPServer(("localhost", 8000), Handler)
+print("Server running on http://localhost:8000")
+server.serve_forever()
