681c89ec47fa48d8975ab57d0af20bb2533eff05
vorw <sultanalsanie23@gmail.com>
2025-09-06 20:24:05 +0000

    Task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- database and cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	FOREIGN KEY(state_id) REFERENCES states(id)
+);
