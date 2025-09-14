418d39a174599cd506f7315c338e4957a74e0c55
vorw <sultanalsanie23@gmail.com>
2025-09-14 15:51:41 +0000

    Task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+SQL Injection...
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        host="localhost",
+        port=3306)
+
+    cursor = database.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4],)
+    )
+    output = cursor.fetchall()
+    for rows in output:
+        print(rows)
+    cursor.close()
+    database.close()
+
