d506936112a396ce01cf824fa3419201e54fae01
vorw <sultanalsanie23@gmail.com>
2025-09-14 15:32:40 +0000

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+lists all states where name matches the user argument
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            host="localhost",
+            port=3306)
+
+    cursor = database.cursor()
+    cursor.execute(
+            "SELECT * FROM states WHERE name = '{}' "
+            "ORDER BY id ASC".format(argv[4]))
+    output = cursor.fetchall()
+    for rows in output:
+        print(rows)
+    cursor.close()
+    database.close()
