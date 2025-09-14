223993d2a4f89b67c47d0bd033b21b7554f5894f
vorw <sultanalsanie23@gmail.com>
2025-09-14 15:42:07 +0000

    Task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,23 +3,23 @@
 Filter states by user input
 """
 
-
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
     database = MySQLdb.connect(
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            host="localhost",
-            port=3306)
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        host="localhost",
+        port=3306
+    )
 
     cursor = database.cursor()
     cursor.execute(
         ("SELECT * FROM states WHERE name = '{}' "
-         "ORDER BY id ASC").format(argv[4]))
+         "ORDER BY id ASC").format(argv[4])
+    )
     output = cursor.fetchall()
     for rows in output:
         print(rows)
